077c9d59a4fab8dcded74a96d3abc848b76b18df
Fernando Carminatti <fernando.carminatti@gmail.com>
2025-11-13 13:29:11 -0300

    Update application configuration and enhance project setup
    - Refactor `application.yml` for updated property structures and additional environment variables.
    - Add `@EntityScan`, `@EnableJpaRepositories`, and `@EnableAsync` annotations to `CineminhaApplication`.
    - Update `.gitignore` to exclude `.media` directory.

--- src/main/java/com/pssa/cineminha/CineminhaApplication.java
+++ src/main/java/com/pssa/cineminha/CineminhaApplication.java
@@ -2,12 +2,16 @@ package com.pssa.cineminha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.pssa.cineminha.entity")
+@EnableJpaRepositories(basePackages = "com.pssa.cineminha.repository")
+@EnableAsync
 public class CineminhaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CineminhaApplication.class, args);
 	}
-
 }
